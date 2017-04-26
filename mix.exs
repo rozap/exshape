c8807abf61d94751bd3cd82a82a34636e3b6248d
Chris Duranti <chrisd1891@gmail.com>
2017-04-26 09:13:21 -0700

    oops

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exshape.Mixfile do
 
   def project do
     [app: :exshape,
-     version: "0.2.2",
+     version: "0.2.4",
      elixir: "~> 1.4",
      description: description(),
      package: package(),
