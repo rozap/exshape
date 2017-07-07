1b8793860687a0aeedbe333957520446a7090ed0
Chris Duranti <chrisd1891@gmail.com>
2017-07-07 15:28:02 -0700

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exshape.Mixfile do
 
   def project do
     [app: :exshape,
-     version: "1.0.1",
+     version: "1.0.2",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
